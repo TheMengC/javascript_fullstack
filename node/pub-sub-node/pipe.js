ea07cc32aa1fc02f1faa2643478e4504d13dc36f
TheMengC <907989525@qq.com>
2019-10-31 17:03:22 +0800

    node提交与下载 pipe

--- node/pub-sub-node/pipe.js
+++ node/pub-sub-node/pipe.js
@@ -1,4 +1,12 @@
 const fs = require('fs')
+fs.readFile('./index.html', (err, data) => {
+  //data 内存
+  fs.writeFile('./index-copy.html', data, (err, res) => {
+    if(!err) {
+      console.log('writed!!!')
+    }
+  })
+})
 // stream
 fs.createReadStream('./music.js')
 .pipe(
