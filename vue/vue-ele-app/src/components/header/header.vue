82a71a7a6dbb8673574f4765e91b42554cb0671b
TheMengC <907989525@qq.com>
2019-11-01 17:02:44 +0800

    饿了么 第二次提交

--- vue/vue-ele-app/src/components/header/header.vue
+++ vue/vue-ele-app/src/components/header/header.vue
@@ -1,15 +1,101 @@
 <template>
   <div class="header">
-    这是头部内容
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img src="./brand@2x.png" alt="">
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">粥品香坊(回龙观)</span>
+        </div>
+        <div class="description">蜂鸟专送/38分钟送达</div>
+        <div class="support">
+          <span class="icon special"></span>
+          <span class="text">在线支付满28减5</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  created () {
+    this.$http.get('http://ustbhuangyi.com/sell/api/seller')
+      .then((res) => {
+        console.log(res)
+      })
+  }
 }
 </script>
 
-<style>
+<style lang='stylus' scoped>
+@import '../../common/stylus/mixin'
+.header
+  position relative
+  color #fff
+  background rgba(7, 17, 27, 0.5)
+  .content-wrapper
+    position relative
+    padding 24px 12px 18px 24px
+    font-size 0
+    .avatar
+      display inline-block
+      vertical-align top
+      img
+        border-radius 2px
 
+    .content
+      display inline-block
+      font-size 14px
+      margin-left 16px
+      .title
+        margin 2px 0 8px 0
+        .brand
+          height 18px
+          width 30px
+          display inline-block
+          bg-image('./brand')
+          background-size 30px 18px
+          background-repeat no-repeat
+          vertical-align middle
+
+        .name
+          vertical-align middle
+          margin-left 6px
+          font-size 16px
+          line-height 18px
+          font-weight bold
+      .description
+        margin-bottom 10px
+        line-height 12px
+        font-size 12px
+      .support
+        .icon
+          display inline-block
+          vertical-align top
+          width 12px
+          height 12px
+          margin-right 4px
+          background-size 12px 12px
+          background-repeat no-repeat
+
+          &.decrease
+            bg-image('decrease_1')
+
+          &.discount
+            bg-image('discount_1')
+
+          &.guarantee
+            bg-image('guarantee_1')
+
+          &.invoice
+            bg-image('invoice_1')
+
+          &.special
+            bg-image('special_1')
+        .text
+          line-height 12px
+          font-size 10px
 </style>
