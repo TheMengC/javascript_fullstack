b239a6753f2a88d098e9af5b7a2427b85f6154c2
TheMengC <907989525@qq.com>
2019-11-02 12:06:25 +0800

    饿了么打二次提交

--- vue/vue-ele-app/src/App.vue
+++ vue/vue-ele-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <!-- 头部 -->
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
     <router-view/>
   </div>
 </template>
@@ -10,8 +10,24 @@
 import header from '@/components/header/header.vue'
 export default {
   name: 'App',
+  data () {
+    return {
+      seller: {}
+    }
+  },
   components: {
     'v-header': header
+  },
+  created () {
+    this.$http.get('http://localhost:8080/static/seller.json')
+      .then((res) => {
+        console.log(res)
+        if (res.data.errno === 0) {
+          // this.seller = res.data.data
+          // 数组合并用concat(), 对象合并用Object.assigin()
+          this.seller = Object.assign({}, this.seller, res.data.data)
+        }
+      })
   }
 }
 </script>
