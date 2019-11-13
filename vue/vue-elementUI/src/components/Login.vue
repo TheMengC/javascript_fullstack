b3f4ee36f5a99a6646bde6bbf8e0cc607213d5b5
TheMengC <907989525@qq.com>
2019-11-13 21:31:00 +0800

    element ui 使用

---
+++ vue/vue-elementUI/src/components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="login">
+    <div class="login-wrapper">
+      <h4>吊炸天管理系统</h4>
+      <el-input v-model="user" placeholder="请输入内容" class="user int"></el-input>
+      <el-input placeholder="请输入密码" v-model="passwaord" show-password class="psd int"></el-input>
+      <el-button el-button type="primary" class="int" @click="login">登录</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      user: '',
+      passwaord: ''
+    }
+  },
+  methods: {
+    login () {
+      if (this.user !== '' && this.passwaord !== '') {
+        localStorage.setItem('name', this.user)
+        this.$router.push({path: '/main'})
+      } else {
+        this.$message.error(' 账号或密码不能为空 ')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.login{
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+}
+.login-wrapper{
+  width: 350px;
+  height: 350px;
+  text-align: center;
+  box-shadow: 0 0 10px #aaa;
+  border-radius: 10px;
+}
+.int{
+  width: 80%;
+  margin: 0 auto;
+  display: block;
+  margin-bottom: 30px;
+}
+</style>
---
+++ vue/vue-elementUI/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import main from '@/components/main'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component: main
+    }
+  ]
+})
