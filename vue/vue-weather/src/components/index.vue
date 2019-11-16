1f4b337766a3183f94dccb8caf5e69c08b7752bd
TheMengC <907989525@qq.com>
2019-11-16 12:04:37 +0800

    天气预报小程序

--- vue/vue-weather/src/components/index.vue
+++ vue/vue-weather/src/components/index.vue
@@ -6,52 +6,175 @@
       <p style="height: 21px">{{localTime}}</p>
       <div class="city-info">
         <dl>
-          <dt class="font18">南昌市</dt>
+          <dt class="font18">{{cityData.city}}</dt>
         </dl>
         <dl>
-          <dt>晴</dt>
+          <dt>{{cityData.weather}}</dt>
         </dl>
         <dl>
-          <dt class="font45">0℃</dt>
+          <dt class="font45">{{cityData.temperature}}℃</dt>
         </dl>
         <dl>
-          <dt>风力：3</dt>
+          <dt>风力：{{cityData.windPower}} | 风向：{{cityData.windDirection}} | 空气湿度：{{cityData.humidity}}</dt>
         </dl>
       </div>
     </div>
+    <div class="feature">
+      <div class="group" v-if="futureTem && futureTem[1]">
+        明日：
+        <span class="tm">白天：{{futureTem[1].dayTemp}} {{futureTem[1].dayWeather}} {{futureTem[1].dayWindDir}} {{futureTem[1].dayWindPower}}</span>
+        <span class="tm">夜间：{{futureTem[1].nightTemp}} {{futureTem[1].nightWeather}} {{futureTem[1].nightWindDir}} {{futureTem[1].nightWindPower}}</span>
+
+      </div>
+      <div class="group" v-if="futureTem && futureTem[2]">
+        后天：
+        <span class="tm">白天：{{futureTem[2].dayTemp}} {{futureTem[2].dayWeather}} {{futureTem[2].dayWindDir}} {{futureTem[2].dayWindPower}}</span>
+        <span class="tm">夜间：{{futureTem[2].nightTemp}} {{futureTem[2].nightWeather}} {{futureTem[2].nightWindDir}} {{futureTem[2].nightWindPower}}</span>
+
+      </div>
+    </div>
+
+    <div class="echart-container" ref="echartContainer"></div>
+
+    <div class="map-container" ref="mapContainer"></div>
 
     <div class="loading" v-show="loader">
       <div class="loader">
-
         <div class="face">
           <div class="circle"></div>
         </div>
-
         <div class="face">
           <div class="circle"></div>
         </div>
-
       </div>
+    </div>
+
+    <div class="select-city-box">
+      <van-area :area-list="areaList" :columns-num="2" title="选择城市"/>
     </div>
   </div>
 </template>
 
 <script>
+import AreaList from './area'
 export default {
-  data () {
+  data() {
     return {
-      loader: false,
-      localTime: ''
-    }
+      loader: true,
+      localTime: "",
+      cityData: {},
+      futureTem: [],
+      seriesData: [],
+      areaList: AreaList
+    };
   },
-  created () {
+  created() {
     setInterval(() => {
-      this.localTime = this.getLocalTime()
-    }, 1000)
+      this.localTime = this.getLocalTime();
+    }, 1000);
+  },
+  mounted() {
+    this.initMap();
   },
   methods: {
-    getLocalTime () {
-      return new Date().toLocaleTimeString()
+    getLocalTime() {
+      return new Date().toLocaleTimeString();
+    },
+    initMap() {
+      let _self = this;
+      var map = new AMap.Map(this.$refs.mapContainer, {
+        resizeEnable: true
+      });
+      AMap.plugin("AMap.CitySearch", function() {
+        var citySearch = new AMap.CitySearch();
+        citySearch.getLocalCity(function(status, result) {
+          if (status === "complete" && result.info === "OK") {
+            // 查询成功，result即为当前所在城市信息
+            console.log(result);
+            _self.getCurrentCityData(result.city)
+          }
+        });
+      });
+    },
+    getCurrentCityData(cityName) {
+      let _self = this;
+      //加载天气查询插件
+      AMap.plugin("AMap.Weather", function() {
+        //创建天气查询实例
+        var weather = new AMap.Weather();
+
+        //执行实时天气信息查询
+        weather.getLive(cityName, function(err, data) {
+          console.log(err, data);
+          _self.cityData = data;
+        });
+        //执行实时天气信息查询
+        weather.getForecast(cityName, function(err, data) {
+            console.log(err, data);
+            _self.futureTem = data.forecasts
+            _self.futureTem.map((item, index) => {
+              _self.seriesData.push(item.dayTemp)
+            })
+            _self.loader = false
+            _self.initEchart()
+        });
+      });
+    },
+    initEchart () {
+      let dom = this.$refs.echartContainer
+      var myChart = echarts.init(dom)
+      let app = {}, option = null;
+      option = {
+        xAxis: {
+            show:true,
+            splitLine: {show: false},
+            type: 'category',
+            data: ['今天', '明天', '后天', '三天后'],
+            axisLine: {
+              lineStyle: {
+                color: '#fff'
+              }
+            },
+            axisTick: {
+              show: false
+            }
+        },
+        yAxis: {
+            show:false,
+            axisLine: {
+              show: false,
+              lineStyle: {
+                color: '#fff'
+              }
+            },
+            axisTick: {show: true},
+            splitLine: {show: false}
+        },
+        tooltip: {
+          trigger: 'axis',
+          formatter: function (params) {
+            var relVal = params[0].name
+            for (let i = 0, l = params.length; i < l; i++) {
+              relVal +=params[i].value + '℃'
+            }
+            return relVal
+          }
+        },
+        legend: {
+          data: ['气温']
+        },
+        series: [{
+            data: this.seriesData,
+            type: 'line',
+            label: {
+              normal: {
+                show: true,
+                position: 'top'
+              }
+            }
+        }]
+      };
+      myChart.setOption(option, true)
     }
   }
 }
@@ -193,7 +316,7 @@ export default {
     color: #fff;
     font-size: 12px;
 }
-.echart-contaier{
+.echart-container{
     width: 100%;
     height: 50vh;
 }
--- vue/vue-weather/src/main.js
+++ vue/vue-weather/src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { Area } from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Area);
 
 Vue.config.productionTip = false
 
