b1e08a4d5930152ba2166cabfdf948b4aa44c43e
TheMengC <907989525@qq.com>
2019-11-28 21:44:51 +0800

    vue watch 实现原理

---
+++ JS/vue-watch-theory/index.js
@@ -0,0 +1,16 @@
+(function() {
+  var o = {a: 1}
+  Object.defineProperty(o, 'b', {
+    get() {
+      return this.a
+    },
+    set(newVal) {
+      this.a = newVal
+    },
+    configurable: true //b 是否可以被再次配置
+  })
+
+  console.log(o.b)
+  o.b = 2
+  console.log(o.b)
+})()
