2c7c76f66a2f2338b0445c7d40f5837f2eb4da67
menglong157 <907989525@qq.com>
2019-11-30 17:09:50 +0800

    滴滴打车微信小程序

--- wxapp/wxdidi/pages/index/index.js
+++ wxapp/wxdidi/pages/index/index.js
@@ -1,23 +1,30 @@
 Page({
   data: {
     // 写死？
-    // easy-mock?
+    // easy mock? 
     // json server
     navData: [],
     currentTab: 0,
-    navScrollLeft: 300
+    navScrollLeft: 500,
+    address: '范家新村',
+    destination: '万达',
+    isLoading: true
   },
   onLoad() {
-    this.requestCart()
+    var w = wx.getSystemInfoSync().windowWidth;
+    this.setData({
+      windowWidth: w
+    })
+    this.requestCart();
   },
-  // 一个请求 一个函数
+  // 一个请求一个函数
   requestCart() {
     wx.request({
       url: 'http://localhost:3000/indexPage',
       success: (res) => {
         // console.log(this)
         // console.log(res.data)
-        // console.log(res);
+        console.log(res);
         const navData = res.data.navData;
         const imgUrls = res.data.imgUrls;
         // const cost = res.data.data.cost
@@ -30,13 +37,16 @@ Page({
       }
     })
   },
-  switchNav(event) {
+  switchNav (event) {
     const cart = event.currentTarget.dataset.name;
     const cur = event.currentTarget.dataset.current;
     let text = this.data.navData;
+    var singleNavWidth = this.data.windowWidth / 6;
+    // console.log((cur-1)*singleNavWidth);
     this.setData({
       cart,
-      currentTab: cur
+      currentTab: cur,
+      navScrollLeft: (cur-1)*singleNavWidth
     })
   }
 })
