e7f3b0a7b4be214f1048618260e3a63eedd88e11
menglong157 <907989525@qq.com>
2019-12-03 16:23:40 +0800

    mixins的应用

---
+++ vue/vue-mixin-drag-loading/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- <HelloWorld/> -->
+    <Dialog/>
+    <Loading />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Dialog from './components/Dialog'
+import Loading from './components/Loading'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Dialog,
+    Loading
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
