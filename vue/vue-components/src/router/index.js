9720d6d72b62b37c6d4f24a2a70032f3b1e122f5
menglong157 <907989525@qq.com>
2019-12-10 21:28:39 +0800

    button组件 以及 provide与reject的使用 dispatch(派发)和broadcast(广播)

--- vue/vue-components/src/router/index.js
+++ vue/vue-components/src/router/index.js
@@ -3,15 +3,15 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import A from '@/components/pages/A'
 import Parent from '@/components/views/parent'
-
+import Show from '@/components/form/show'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Parent',
-      component: Parent
+      name: 'Show',
+      component: Show
     }
   ]
 })
