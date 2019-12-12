0f34bacad9906f374e9670afe338eca0f0c4275e
menglong157 <907989525@qq.com>
2019-12-12 21:39:57 +0800

    button组件 以及 provide与reject的使用 dispatch(派发)和broadcast(广播) $mount $toast实现原理

--- vue/vue-components/src/components/display/display.js
+++ vue/vue-components/src/components/display/display.js
@@ -21,7 +21,7 @@ function str32 () {
   let maxPos = $chars.length
   let str = ''
   for (let i = 0; i < 32; i ++) {
-    str += $chars.charAt(Math.floor(Math.random() * maxPos))
+    str += $chars.charAt(Math.floor(Math.random() * maxPos)) 
   }
   return str
 }
