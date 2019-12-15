0847c6526d104bcba8fe71c2abe61a4d2e7d1ecc
menglong157 <907989525@qq.com>
2019-12-15 16:41:05 +0800

    npm init webpack实现原理

---
+++ node/my-cli/bin/mycli.js
@@ -0,0 +1,13 @@
+#!usr/bin/env node
+// shebang: #! 用于启动该文件的一个环境
+// node app.js
+// live-server
+// vue init abc
+// vue --version
+const commander = require('commander');
+commander.version('1.0.0')
+.usage('<command> [项目名称]')
+.command('init', 'init project')
+.command('hello', 'hello')
+.parse(process.argv)
+// node app.js  init webpack --version
---
+++ node/my-cli/readme.md
@@ -0,0 +1,8 @@
+## 需求
+vue init projectName
+1. 下载 git 
+2. 收集配置
+3. 生成文件
+
+## 3. 生成文件
+模板 (template headerBars) 里面  先写好判断
