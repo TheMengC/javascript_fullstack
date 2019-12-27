1d5418d75e2d3ccf909aa022b6c9f19545e315f1
menglong157 <907989525@qq.com>
2019-12-27 11:35:51 +0800

    缓存

--- JS/fp-memorize-http-cache/http-memorize/app.js
+++ JS/fp-memorize-http-cache/http-memorize/app.js
@@ -1,39 +1,57 @@
 var Koa = require('koa');
 var Router = require('koa-router');
-var koaStatic = require('koa-static');
-
+const md5 = require('md5');
+ 
 var app = new Koa();
 var router = new Router();
 
-// app.use(koaStatic(__dirname))
-// 把磁盘上的文件路径  映射为  网络url
-// app1.js   localhost:8090/app1.js
 
+// 把 磁盘上的文件路径 映射为 网络url
+// app1.js    localhost:9999/app1.js
 router.get('/', (ctx, next) => {
   // ctx.router available
-  ctx.body =( `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Document</title>
-    </head>
-    <body>
-      <div id="app"></div>
-      <script src="/app1.js"></script>
-    </body>
-    </html>
-  `)
+  ctx.body = `
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <div id="app"></div>
+  <script src="/app1.js"></script>
+</body>
+</html>
+  `
 });
 router.get('/app1.js', async (ctx) => {
-  console.log('app1.js 请求')
-  const fs = require('fs')
-  const content = fs.readFileSync('./app1.js', 'utf-8')
-  const time = Date.now() + 1000 * 30 // 30s
-  ctx.set('cache-control', 'public, max-age=30') //30s a-> serverA -> serverB
-  ctx.body = content
+  console.log('app1.js 请求');
+  const fs = require('fs');
+  const content = fs.readFileSync('./app1.js', 'utf8');
+  const stat = fs.statSync('./app1.js');
+  const etag = md5(content);
+  const time = Date.now() + 1000 * 30; // 30s
+  // 
+  if (ctx.req.headers['if-none-match']
+  === etag
+  ) {
+    ctx.status = 304
+    ctx.body = ''
+    return;
+  }
+  if (ctx.req.headers['if-modified-since']
+  === stat.mtime
+  ) {
+    ctx.status = 304;
+    ctx.body = '';
+    return;
+  }
+  ctx.set('cache-control', 'public,max-age=30'); // 30s a -> ServerA -> ServerB
+  ctx.set('ETag', etag);
+  ctx.set('Last-Modified', stat.mtime);
+  ctx.body = content;
 })
 app
   .use(router.routes())
