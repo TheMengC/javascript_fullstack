1e697510edc27e35153029307b88cd3890e2d72d
menglong157 <907989525@qq.com>
2020-02-17 15:48:48 +0800

    react 基础

--- react-v16/learn/src/index.js
+++ react-v16/learn/src/index.js
@@ -13,40 +13,139 @@ import ReactDOM from 'react-dom'
 
 // state
 // 生命周期
-class Counter extends React.Component {
-  constructor (props) {
+// class Counter extends React.Component {
+//   constructor (props) {
+//     super(props)
+//     // 初始化构造器时设置内部状态 num值为1
+//     this.state = {
+//       num: 1
+//     }
+//     this.handleClick = this.handleClick.bind(this)
+//   }
+//   // 生命周期方法
+//   componentDidMount() {
+//     console.log('componentDidMount  函数触发')
+//   }
+//   // 避免组件重复或者无意义的渲染
+//   shouldComponentUpdate(nextProps, nextState) {
+//     // console.log(nextProps, nextState)
+//     if(nextState.num % 2) {
+//       return true
+//     }
+//     return false
+//   }
+//   handleClick() {
+//     this.setState({
+//       num: this.state.num +1
+//     })
+//   }
+//   render () {
+//     return (
+//       <div>
+//         <p>{this.state.num}</p>
+//         <button onClick={this.handleClick}>click</button>
+//       </div>
+//     )
+//   }
+// }
+
+// 表单
+// 渲染列表
+// class TodoList extends React.Component {
+//   constructor (props) {
+//     super(props)
+//     this.state = {
+//       text: '帅气的蜗牛',
+//       todos: ['Learn React', 'Learn-Ant-design', 'Learn Koa']
+//     }
+//     this.handleChange = this.handleChange.bind(this)
+//     this.handleClick = this.handleClick.bind(this)
+//   }
+//   handleChange (e) {
+//     console.log(e.target.value)
+//     this.setState({
+//       text: e.target.value
+//     })
+//   }
+//   handleClick() {
+//     if (this.state.text) {
+//       this.setState(state => ({
+//         // 将text添加到todos数组里面
+//         todos: [...state.todos, state.text],
+//         text: ''
+//       }))
+//     }
+    
+//   }
+//   render () { // jsx
+//     return (
+//       <div>
+//         {this.state.text}
+//         <input type="text" value={this.state.text} onChange={this.handleChange} />
+//         <button onClick={this.handleClick}>add</button>
+//         <ul>
+//           {this.state.todos.map( v => {
+//             return <li key={v}>{v}</li>
+//           })}
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+// React 16 新增
+// 1. render 函数支持返回数组和字符串
+// 2. 异常处理，添加componentDidCatch钩子 (函数) 获取组件错误
+
+class React16 extends React.Component {
+  constructor(props) {
     super(props)
-    // 初始化构造器时设置内部状态 num值为1
     this.state = {
-      num: 1
+      hasError: false
     }
-    this.handleClick = this.handleClick.bind(this)
-  }
-  // 生命周期方法
-  componentDidMount() {
-    console.log('componentDidMount  函数触发')
-  }
-  // 避免组件重复或者无意义的渲染
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   // console.log(nextProps, nextState)
-  //   if(nextState.num % 2) {
-  //     return true
-  //   }
-  //   return false
-  // }
-  handleClick() {
-    this.setState({
-      num: this.state.num +1
-    })
   }
   render () {
     return (
       <div>
-        <p>{this.state.num}</p>
-        <button onClick={this.handleClick}>click</button>
+        {this.state.hasError ? <div>出错了</div> : null}
+        <ClickWriteError />
+        <FeatureReturnFraments /> 
       </div>
     )
   }
 }
 
-ReactDOM.render(<Counter/>, document.querySelector('#root'))
+// ClickWriteError 是一个组件
+class ClickWriteError extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: false
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick () {
+    this.setState({
+      error: true
+    })
+  }
+  render () {
+    if(this.state.error) {
+      throw new Error ('出错了')
+    }
+    return <button onClick={this.handleClick}>抛出错误</button>
+  }
+}
+
+class FeatureReturnFraments extends React.Component {
+  render() {
+    return [
+      <p key="key1">React 很不错</p>,
+      '文本1',
+      <p key="key2">Antd也很赞</p>,
+      '文本2'
+    ]
+  }
+}
+
+ReactDOM.render(<React16/>, document.querySelector('#root'))
